9fb629d8bac6a6dd52e45f7e14d61ecb39b1254b
Roberto Molina <roberto.molina@nan-labs.com>
2024-01-22 09:39:51 -0300

    update eslint config

--- examples/react-native-expo-router/.eslintrc.js
+++ examples/react-native-expo-router/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: ['universe/native', 'plugin:prettier/recommended'],
+  extends: ['universe/native', 'universe/shared/typescript-analysis', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
 };
